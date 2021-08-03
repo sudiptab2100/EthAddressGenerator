56140d0ff5283697d7997347a332fa5d18eacccb
Sudipta <sudiptab2100@gmail.com>
2021-08-03 22:01:21 +0530

    Duplicate Checker

--- check_address.py
+++ check_address.py
@@ -1,3 +1,4 @@
+from coincurve import keys
 from web3 import HTTPProvider, Web3
 import web3
 from address_generator import genPrivateKey, privateKeyToEthAddress
@@ -5,17 +6,18 @@ from address_generator import genPrivateKey, privateKeyToEthAddress
 w3 = Web3(HTTPProvider('https://bsc-dataseed.binance.org/'))
 MAX_LIMIT = 200000
 count = 0
-
+duplicate = 0
+addr_used = {}
 while(count < MAX_LIMIT):
-    print(count)
-    count += 1
 
     priv = genPrivateKey()
-    addr = Web3.toChecksumAddress('0x' + privateKeyToEthAddress(priv).hex())
+    addr_str = '0x' + privateKeyToEthAddress(priv).hex()
+    addr = Web3.toChecksumAddress(addr_str)
+    if addr_str in addr_used.keys():
+        duplicate += 1
+        continue
+    addr_used[addr_str] = True
     balance = w3.eth.get_balance(addr)
-    # print(addr)
-    # print(balance)
-    # print(type(balance))
 
     if(balance > 0):
         print()
@@ -23,4 +25,7 @@ while(count < MAX_LIMIT):
         print(addr)
         print(balance)
         break
+
+    print("Count:", count, "Duplicate:", duplicate)
+    count += 1
 
